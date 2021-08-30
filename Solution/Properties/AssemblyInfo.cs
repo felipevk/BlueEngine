b488468a8faa39e81c2f2c8e89b9f5cd21afbf56
Pedro Kauati <felipevk@gmail.com>
2021-08-29 17:46:58 -0700

    Reorganized solution

--- Solution/Properties/AssemblyInfo.cs
+++ Solution/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BlueEngine")]
+[assembly: AssemblyTitle("Solution")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BlueEngine")]
-[assembly: AssemblyProduct("BlueEngine")]
+[assembly: AssemblyCompany("Solution")]
+[assembly: AssemblyProduct("Solution")]
 [assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
